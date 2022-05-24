049b278dbbde9b76ee473a6272bd8e8ae1456ea4
fredriksommer <fredris@hiof.no>
2022-05-24 12:48:12 +0200

    return data as hashmap with strings

--- src/main/java/simplewebscraper/datadocument/DataDocument.java
+++ src/main/java/simplewebscraper/datadocument/DataDocument.java
@@ -5,43 +5,49 @@ import simplewebscraper.HtmlElement;
 import simplewebscraper.HtmlElements;
 import simplewebscraper.InvalidColumnException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DataDocument {
     DataDocumentBackend backend;
 
     HashMap<String, HtmlElements> fields;
 
+
     public DataDocument(DataDocumentBackend backend) {
         this.backend = backend;
         this.fields = new HashMap<>();
     }
 
-    public void addField(String fieldName, String xpath) {
+    public void addField(String fieldName, String xpath) throws InvalidColumnException {
         if (!fields.containsKey(fieldName))
             fields.put(fieldName, backend.getData(xpath));
         else
-            try {
-                throw new InvalidColumnException("Field already exists");
-            } catch (InvalidColumnException e) {
-                e.printStackTrace();
-            }
+            throw new InvalidColumnException("Field already exists");
+
     }
 
-    public void addField(String fieldName, DataCollector collector) {
+    public void addField(String fieldName, DataCollector collector) throws InvalidColumnException {
         if (!fields.containsKey(fieldName))
             fields.put(fieldName, backend.getData(collector));
         else
-            try {
-                throw new InvalidColumnException("Field already exists");
-            } catch (InvalidColumnException e) {
-                e.printStackTrace();
-            }
+            throw new InvalidColumnException("Field already exists");
+
     }
 
     // Test
-    public HashMap<String, HtmlElements> getCollection() {
-        return fields;
+    public HashMap<String, List<String>> getCollection() {
+        HashMap<String, List<String>> documentData = new HashMap<>();
+        fields.forEach(
+                (key, value)
+                        -> {
+                    if (!documentData.containsKey(key))
+                        documentData.put(key, new ArrayList<>());
+                        documentData.get(key).addAll(fields.get(key).toListAsString());
+                }
+        );
+        return documentData;
     }
 
     // For testing only
