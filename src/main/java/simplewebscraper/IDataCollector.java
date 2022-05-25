dc1c55f844e74134f9ee57565b74102e2882f4f2
fredriksommer <fredris@hiof.no>
2022-05-25 18:29:24 +0200

    JavaDoc IDataCollector

--- src/main/java/simplewebscraper/IDataCollector.java
+++ src/main/java/simplewebscraper/IDataCollector.java
@@ -1,5 +1,14 @@
 package simplewebscraper;
 
+/**
+ * The interface Data collector.
+ */
 public interface IDataCollector {
+    /**
+     * Collect HtmlElements by using lambda expression.
+     *
+     * @param scraper the scraper
+     * @return the html elements
+     */
     HtmlElements collectHtmlElements(WebScraper scraper);
 }
