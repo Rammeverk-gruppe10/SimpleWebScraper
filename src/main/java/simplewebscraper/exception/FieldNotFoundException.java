02dba5071574e1b985ff8f07d1b99ce175e77b87
fredriksommer <fredris@hiof.no>
2022-05-25 18:20:38 +0200

    Javadoc FieldNotFoundException

--- src/main/java/simplewebscraper/exception/FieldNotFoundException.java
+++ src/main/java/simplewebscraper/exception/FieldNotFoundException.java
@@ -1,6 +1,14 @@
 package simplewebscraper.exception;
 
+/**
+ * The type Field not found exception.
+ */
 public class FieldNotFoundException extends Exception {
+    /**
+     * Instantiates a new Field not found exception.
+     *
+     * @param message the message
+     */
     public FieldNotFoundException(String message) {
         super(message);
     }
